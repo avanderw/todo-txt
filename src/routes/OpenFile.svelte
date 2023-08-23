e275329bbe440b4a94e162abbc882749f8f34d8e
Andrew van der Westhuizen <avanderw@gmail.com>
2023-08-23 10:54:50 +0200

    Add open/save to the filesystem

--- src/routes/OpenFile.svelte
+++ src/routes/OpenFile.svelte
@@ -1,24 +1,61 @@
+<script>
+	// @ts-nocheck
+	import { browser } from '$app/environment';
 
+	let info;
+	if (browser && 'showOpenFilePicker' in window) {
+		info = 'showOpenFilePicker is supported';
+	} else {
+		info = 'showOpenFilePicker is not supported, use an alternative';
+	}
+
+    let fileHandle;
+	let file;
+	let text;
+	async function openFile() {
+		[fileHandle] = await window.showOpenFilePicker({ multiple: false });
+		file = await fileHandle.getFile();
+		text = await file.text();
+	}
+
+	if (browser) {
+		const body = document.querySelector('body');
+		window.addEventListener('dragover', (e) => {
+			body.classList.add('dragover');
+			e.preventDefault();
+			e.stopPropagation();
+		});
+		window.addEventListener('dragleave', (e) => {
+			body.classList.remove('dragover');
+		});
+		window.addEventListener('drop', async (e) => {
+			body.classList.remove('dragover');
+			e.preventDefault();
+			e.stopPropagation();
+			file = e.dataTransfer.files[0];
+            fileHandle = await e.dataTransfer.items[0].getAsFileSystemHandle();
+			console.log(file);
+			text = await file.text();
+		});
+	}
+
+    async function saveFile() {
+        const writable = await fileHandle.createWritable();
+        await writable.write(text);
+        await writable.close();
+    }
+</script>
 
 <p>{info}</p>
 {#if file}
-    <p>{file.name}</p>
+	<ul>
+		<li>{file.lastModifiedDate}</li>
+		<li>{file.name}</li>
+		<li>{file.size}</li>
+		<li>{file.type}</li>
+	</ul>
+	<p>{text}</p>
 {/if}
 
-<button on:click={openFile}>open</button><script>
-    // @ts-nocheck
-
-    let info;
-    if ('showOpenFilePicker' in window) {
-        info = 'showOpenFilePicker is supported';
-    } else {
-        info = 'showOpenFilePicker is not supported, use an alternative';
-    }
-
-    let file;
-    async function openFile() {
-        const [fileHandle] = await window.showOpenFilePicker({multiple:false});
-        const file = await fileHandle.getFile();
-        console.log(file);
-    }
-</script>
+<button on:click={openFile}>open</button>
+<button on:click={saveFile}>save</button>
