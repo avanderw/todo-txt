5fe351b8a302fb0a07a2f52e38030c35441262b4
Andrew van der Westhuizen <avanderw@gmail.com>
2023-08-23 13:16:14 +0200

    Fix null check with todo view on page load

--- src/routes/TodoView.svelte
+++ src/routes/TodoView.svelte
@@ -3,9 +3,8 @@
     import { file, todoItems } from '$lib/stores';
 </script>
 
-<h1>{$file.name}</h1>
-
 {#if $todoItems}
+    <h1>{$file.name}</h1>
     <h2>Number of todos: {$todoItems.length}</h2>
     {#each $todoItems as todo}
         <p>{todo.render()}</p>
