ae0c336bb253fc872c53949f21a0aa81e15a6179
Andrew van der Westhuizen <avanderw@gmail.com>
2023-08-23 09:37:35 +0200

    Add static site generation confirguration

---
+++ src/routes/OpenFile.svelte
@@ -0,0 +1,24 @@
+
+
+<p>{info}</p>
+{#if file}
+    <p>{file.name}</p>
+{/if}
+
+<button on:click={openFile}>open</button><script>
+    // @ts-nocheck
+
+    let info;
+    if ('showOpenFilePicker' in window) {
+        info = 'showOpenFilePicker is supported';
+    } else {
+        info = 'showOpenFilePicker is not supported, use an alternative';
+    }
+
+    let file;
+    async function openFile() {
+        const [fileHandle] = await window.showOpenFilePicker({multiple:false});
+        const file = await fileHandle.getFile();
+        console.log(file);
+    }
+</script>
