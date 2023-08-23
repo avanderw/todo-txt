a567dd3d9529a032114e6ef1ba073245d805646d
Andrew van der Westhuizen <avanderw@gmail.com>
2023-08-23 23:58:40 +0200

    Add hiding of completed todos

--- src/routes/TodosView.svelte
+++ src/routes/TodosView.svelte
@@ -3,17 +3,30 @@
 	import { file, todoTxt, todoItems, editTodo } from '$lib/stores';
 	import TodoComplete from './TodoComplete.svelte';
 	import TodoDelete from './TodoDelete.svelte';
+
+	let hide = false;
 </script>
 
 {#if $todoItems}
 	<h1>{$file.name}</h1>
-	<h2>Showing {$todoItems.length} of {$todoTxt.length} todos</h2>
+	<h2>
+		Showing {$todoItems.length - (hide ? $todoItems.filter((t) => t.isComplete()).length : 0)} of {$todoTxt.length}
+		todos ({$todoItems.filter((t) => t.isComplete()).length} complete <button on:click={() => (hide = !hide)}>{hide}</button>)
+		
+	</h2>
 	{#each $todoItems as todo}
-		<p>
-			<TodoComplete {todo} />
-			<button on:click={()=>{$editTodo = todo}} class:complete={todo.isComplete()}>{todo.render()}</button>
-			<TodoDelete {todo} />
-		</p>
+		{#if !hide || !todo.isComplete()}
+			<p>
+				<TodoComplete {todo} />
+				<button
+					on:click={() => {
+						$editTodo = todo;
+					}}
+					class:complete={todo.isComplete()}>{todo.render()}</button
+				>
+				<TodoDelete {todo} />
+			</p>
+		{/if}
 	{/each}
 {/if}
 
