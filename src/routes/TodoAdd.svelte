5f6d2cab348d2f3996e3418944d536e4fa0a3133
Andrew van der Westhuizen <avanderw@gmail.com>
2023-08-23 16:32:12 +0200

    Clear add on Escape

--- src/routes/TodoAdd.svelte
+++ src/routes/TodoAdd.svelte
@@ -10,9 +10,15 @@
         value = "";
         $status = "Todo added";
     }
+
+    function clearAdd(e) {
+        if (e.key === "Escape") {
+            value = "";
+        }
+    }
 </script>
 
 <form on:submit={addTodo}>
-    <input type="text" bind:value />
-    <button type="submit">add</button>
+    <input type="text" on:keyup={clearAdd} bind:value />
+    <button type="submit" disabled={!value}>add</button>
 </form>
