54ac2a001cb0440f67e5ce8c381014e8a1bb9dac
Andrew van der Westhuizen <avanderw@gmail.com>
2023-08-23 13:02:33 +0200

    Add todo text filtering

---
+++ src/routes/TodosFilter.svelte
@@ -0,0 +1,20 @@
+<script>
+	//@ts-nocheck
+	let filter;
+
+    import { todoItems, todoTxt } from '$lib/stores';
+
+    function filterItems() {
+        $todoItems = $todoTxt.items().filter((item) => item.render().includes(filter));
+    }
+
+    function clearFilter() {
+        filter = null;
+        $todoItems = $todoTxt.items();
+    }
+</script>
+
+<input on:keyup={filterItems} type="text" bind:value={filter} />
+{#if filter}
+	<button on:click={clearFilter}>clear filter</button>
+{/if}
