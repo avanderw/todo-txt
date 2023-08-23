1cfd9807f2e099f9af28c272985dbbd62692995e
Andrew van der Westhuizen <avanderw@gmail.com>
2023-08-23 14:54:27 +0200

    Add the ability to delete todos

---
+++ src/routes/TodoDelete.svelte
@@ -0,0 +1,12 @@
+<script>
+    // @ts-nocheck
+    import { todoItems, todoTxt } from "$lib/stores";
+    
+    export let todo;
+
+    function deleteTodo() {
+        $todoTxt.removeItem(todo);
+        $todoItems = $todoTxt.items().sort((a, b) => a.render().localeCompare(b.render()));
+    }
+</script>
+<button on:click={deleteTodo}>delete</button>
