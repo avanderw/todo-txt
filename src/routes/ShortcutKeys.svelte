c7d23ca1d1ccdef8c7291273f76b70b7add41664
Andrew van der Westhuizen <avanderw@gmail.com>
2023-09-21 14:35:06 +0200

    Give focus to input when editing todos

--- src/routes/ShortcutKeys.svelte
+++ src/routes/ShortcutKeys.svelte
@@ -1,22 +1,29 @@
 <h1>Shortcut keys</h1>
 <table>
-    <tr>
-        <th>Key</th>
-        <th>Action</th>
-    </tr>
-    <tr>
-        <td>CTRL + S</td>
-        <td>Save</td>
-    </tr>
-    <tr>
-        <td>CTRL + F</td>
-        <td>Search</td>
-    </tr>
+	<tr>
+		<th>Key</th>
+		<th>Action</th>
+	</tr>
+	<tr>
+		<td>CTRL + S</td>
+		<td>Save todo.txt file</td>
+	</tr>
+	<tr>
+		<td>CTRL + F</td>
+		<td>Search todos</td>
+	</tr>
 </table>
 
 <style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
+	h1 {
+		font-size: x-small;
+	}
+    th {
+        text-align: left;
     }
+	table {
+		border-collapse: collapse;
+		width: 100%;
+		font-size: xx-small;
+	}
 </style>
